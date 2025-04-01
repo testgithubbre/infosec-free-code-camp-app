0348aed1604159eadbb2c065d6d017939311598f
Birhanu Bade <badebirhanu@gmail.com>
2025-04-01 15:47:30 +0300

    Update app files, switch to yarn

--- myApp.js
+++ myApp.js
@@ -2,12 +2,7 @@ const express = require('express');
 const helmet = require('helmet');
 const app = express();
 
-const PORT = process.env.PORT || 3030;
 
-
-app.listen(PORT, () => {
-  console.log(`🥦 Useful Programmer info Security App Started on Port ${PORT}`);
-});
 
 
 
@@ -65,5 +60,5 @@ app.get("/", function (request, response) {
 });
 let port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`Your app is listening on port ${port}`);
+  console.log(`🥦 Useful Programmer info Security App Started on Port ${port}`);
 });
